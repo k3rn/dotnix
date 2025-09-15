4a633858de5114d846cae53fecfc7ae97dc787d1
Mateus Kern <kern@mateuskern.com>
2025-09-15 09:42:01 -0300

    add bluetooth

--- configuration.nix
+++ configuration.nix
@@ -59,6 +59,7 @@
   security.soteria.enable = true;
   services.tailscale.enable = true;
   services.gnome.gnome-keyring.enable = true;
+  services.blueman.enable = true;
   services.gvfs.enable = true;
    services.upower = {
 	enable = true;
@@ -79,6 +80,19 @@
     ];
     config.common.default = "wlr";
    };
+   hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
